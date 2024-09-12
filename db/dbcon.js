4c9f6f84db9a515a0b15ead04f45679a5dc445cb
kksharma <kksharma233@gmail.com>
2024-09-12 09:46:21 +0000

    adding mongo uri to env

--- db/dbcon.js
+++ db/dbcon.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConn = async () => {
     try {
-        const dbcond = await mongoose.connect('mongodb+srv://admin:admin@azure-node.sapmj.mongodb.net/?retryWrites=true&w=majority&appName=azure-node');
+        const dbcond = await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Could not connect to MongoDB', err);
